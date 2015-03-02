c53f1923f720c779c35cb442946bdf406f205513
vetaStudent <vetadesk03@mini-de-olivier.intra.vetasoft.com>
2015-03-02 16:20:00 +0100

    Initial Commit

---
+++ VSRestkitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSRestkitTest
+//
+//  Created by vetadesk on 2/03/15.
+//  Copyright (c) 2015 VetasoftStudents. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSRestkitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VSRestkitTest
+//
+//  Created by vetadesk on 2/03/15.
+//  Copyright (c) 2015 VetasoftStudents. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
